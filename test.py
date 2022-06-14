e2a026b3621b2b6ffe0715b24598360eb44b5443
choongsil <s.c.sil35@gmail.com>
2022-06-14 14:39:05 +0900

    2022-06-14 14:40

--- test.py
+++ test.py
@@ -28,8 +28,34 @@ else:
 #         y+=1
 #         if(y==10):
 #             print('---------')
-for i in range(2,10):
-    for y in range(1,10):
-        print(i,'X',y,'=',i*y)
-        if (y==9):
-            print('-----------')
+# for i in range(2,10):
+#     for y in range(1,10):
+#         print(i,'X',y,'=',i*y)
+#         if (y==9):
+#             print('-----------')
+
+# for i in range(1,100):
+#     if(i%2!=0 and i%3!=0):
+#         print(i)
+#
+# i=1
+# while i < 100:
+#     if (i % 2 != 0 and i % 3 == 0):
+#         print(i)
+#     i += 1
+# 연습문제 3-6
+# import random
+# while True:
+#     num=random.randint(1, 10)
+#     i=int(input('1~10사이의 숫자를 입력해주세요'))
+#     if i==num:
+#         print("정답입니다. 정답은%d" %(i))
+#         break
+#   연습문제 3-3
+i=(-1)+3+(-5)+7+(-9)
+y=9
+k=0
+while y<100:
+    k=i+y
+    y+=1
+print(k)
