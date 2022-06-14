3900f9c0e12cf8847221bffb33293c7df4f31cf9
choongsil <s.c.sil35@gmail.com>
2022-06-14 11:08:04 +0900

    2022-06-14 11:07

--- test.py
+++ test.py
@@ -9,6 +9,13 @@
 
 # for i in range(10,1,-1):
 #     print("*"*i)
-y=int(input('단수입력:'))
+'''y=int(input('단수입력:'))
 for i in range(1,10):
-    print(y, 'X', i, '=', y*i)
+    print(y, 'X', i, '=', y*i)'''
+x=int(input('값을 입력하시오.'))
+if x>0:
+    print('x는 양의 정수입니다.')
+elif x<0:
+    print('x는 음의 정수 입니다.')
+else:
+    print('x는 0 입니다.')
