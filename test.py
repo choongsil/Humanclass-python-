b6bcd7879fb1b428be732914002e0918368db538
choongsil <s.c.sil35@gmail.com>
2022-06-14 09:11:24 +0900

    2022-06-14 python

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
-for i in range(2, 10):
-    for y in range(1, 10):
-        print(i, 'X', y, '=', i*y)
-        if(y==9):
-            print('-----')
+# for i in range(2,10):
+#     for y in range(1,10):
+#         print(i, "X", y, "=", i*y)
+#         if(y==9):
+#             print("-----------------")
+
+# for i in range(1, 10):
+#     print("*"*i)
+
+# for i in range(10,1,-1):
+#     print("*"*i)
