a0e5ed586f39b58faffaf4c5f9ffece9e93dbdec
choongsil <s.c.sil35@gmail.com>
2022-06-16 15:35:52 +0900

    text file read and write and update

--- textmanege.py
+++ textmanege.py
@@ -9,10 +9,20 @@
 # f.close()
 
 #readline()사용하기
-f = open('d:/temp/menu.txt', mode='r', encoding='utf-8')
-line=f.readline()
-while line!='':
-    ar=line.split(",")
-    print('메뉴명=', ar[0], ',', '가격=', ar[1], end='')
-    line=f.readline()
+# f = open('d:/temp/menu.txt', mode='r', encoding='utf-8')
+# line=f.readline()
+# while line!='':
+#     ar=line.split(",")
+#     print('메뉴명=', ar[0], ',', '가격=', ar[1], end='')
+#     line=f.readline()
+# f.close()
+
+#데이터 파일 저장하기
+f = open('d:/temp/menu1.txt', mode='w', encoding='utf-8') #없으면 만들어서 열고, 있으면 열고
+f.write("아메리카노")
+f.write("라떼")
+f.close()
+
+f = open('d:/temp/menu1.txt', mode='w', encoding='utf-8') #없으면 만들어서 열고, 있으면 열고
+f.writelines(["아메리카노","라떼"])
 f.close()
