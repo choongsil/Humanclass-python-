1be1a705ae443a6b2cb978bc476a5a6473522c8f
choongsil <s.c.sil35@gmail.com>
2022-06-17 17:11:20 +0900

    웹 크롤링

--- sise.py
+++ sise.py
@@ -1,8 +1,8 @@
 from bs4 import BeautifulSoup # web page analysis
 import urllib.request as req
 
-lname=['삼성전자','현대자동차','sk하이닉스']
-lStock=['005930','005380','000660'] #삼성전자, 현대자동차, sk하이닉스
+lname=['삼성전자','현대자동차','sk하이닉스','엘지에너지솔루션','엘지화학','네이버','삼성SDI']
+lStock=['005930','005380','000660', '373220', '051910', '035420', '006400'] #삼성전자, 현대자동차, sk하이닉스
 i=0
 for x in lStock:
     url="https://finance.naver.com/item/sise.naver?code="+x
@@ -10,5 +10,6 @@ for x in lStock:
     soup=BeautifulSoup(res,"html.parser")
     selector=f'#middle > dl > dd:nth-child(5)'
     datastr=soup.select_one(selector)
-    print(lname[i],',',datastr)
+    txtstr=datastr.text.split(' ')
+    print(lname[i],',',txtstr[1])
     i+=1
